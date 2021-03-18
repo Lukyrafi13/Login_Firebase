e3248487027b26666631781ab7ba73b479ba45eb
Lukyrafi13 <63963416+Lukyrafi13@users.noreply.github.com>
2021-03-18 20:55:44 +0700

    Initial commit

---
+++ app/src/main/java/com/luckyrafi13/login_firebase/MainActivity.java
@@ -0,0 +1,31 @@
+package com.luckyrafi13.login_firebase;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class MainActivity extends AppCompatActivity {
+
+    Button bt_Login, bt_Signup;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        bt_Login = findViewById(R.id.bt_Login);
+        bt_Signup = findViewById(R.id.bt_Signup);
+
+        bt_Login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(MainActivity.this,LoginActivity.class);
+                startActivity(intent);
+            }
+        });
+
+    }
+}
